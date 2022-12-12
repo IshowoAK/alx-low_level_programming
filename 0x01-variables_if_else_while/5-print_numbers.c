49d054bf519cba1e08f132c03f6904b76827e28d
IshowoAK <IshowoAK>
2022-12-12 14:00:01 -0800

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,13 +10,12 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		if ( c != 'e' && c!= 'q')
+		if (c != 'e' && c != 'q')
 		{
-		putchar(c);
+			putchar(c);
 		}
-	c++;
+		c++;
 	}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
-

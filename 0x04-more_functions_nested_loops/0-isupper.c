35bf5f0163d8b64c825fb8307c3fd4343edc740a
IshowoAK <IshowoAK>
2022-12-15 13:36:01 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase letters
+ *  @c: The character to be checked
+ *
+ *  Return: 1 if character is uppercase, 0 if otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && 7 c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ */
+
+int _putchar(char c);
+
+
+
+#endif

135624ef11f3b2d67856b22e9c694f06534f5a8a
IshowoAK <IshowoAK>
2022-12-16 12:10:19 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @b: The first integer to be multiplied is.
+ * @c: The second integer to be multipliedis.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int b, int c)
+{
+	return (b * c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@
 
 int _putchar(char c);
 int _isdigit(int c);
-
+int _isupper(int c);
+int mul(int b, int c),
 
 #endif

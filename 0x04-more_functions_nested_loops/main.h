6830afcdfa43446aba0d3fc16dfb61c622ec1288
IshowoAK <IshowoAK>
2022-12-16 12:23:28 -0800

    numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,9 +5,10 @@
  * File - main.h
  */
 
+int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
-int _isupper(int c);
-int mul(int a, int b),
+int mul(int a, int b);
+void print_numbers(void);
 
 #endif

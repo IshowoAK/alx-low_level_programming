484980c65bb5ee734c296d6bc62ecee6a9526275
IshowoAK <IshowoAK>
2022-12-22 14:29:05 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int x;
 
-	for(x = 1; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x %5 == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else if (x % 3 ==0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
